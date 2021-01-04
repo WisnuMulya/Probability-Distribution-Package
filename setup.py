df75a60f50e7ada4de551c71db9685d24d22de34
Wisnu Mulya <wisnu@wisnumulya.com>
2021-01-05 00:28:14 +0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name= "distribution",
+      version = "0.1",
+      description = "Package of Statistical Distributions",
+      packages=["distribution"],
+      zip_safe = False)
