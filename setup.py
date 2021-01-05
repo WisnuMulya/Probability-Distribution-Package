fbf4699c75ea97d668aca6ee3d83d9a0fafbab3a
Wisnu Mulya <wisnu@wisnumulya.com>
2021-01-05 19:47:06 +0700

    Add long description to setup.py from the README.md

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
 setup(name= "wpm_probability",
       version = "0.1",
       description = "Probability distributions objects: Gaussian, Binomial.",
+      long_description= long_description,
+      long_description_content_type = "text/markdown",
       author = "Wisnu Mulya",
       author_email = "wisnu@wisnumulya.com",
       url="https://github.com/WisnuMulya/Probability-Distribution-Package",
